f3b24cac667188f1c4c8a4ca706fe93ad8b100ec
student <mwoolery@nw40951.nwmissouri.edu>
2017-11-15 15:13:23 -0600

    drew tic tac board and things for x's and o's

--- Ready, Set, Draw/ClockViewController.swift
+++ Ready, Set, Draw/ClockViewController.swift
@@ -10,9 +10,7 @@ import UIKit
 
 // see this blogpost: http://sketchytech.blogspot.co.uk/2014/11/swift-how-to-draw-clock-face-using_12.html
 
-enum NumberOfNumerals:Int {
-    case two = 2, four = 4, twelve = 12
-}
+
 class ClockViewController: UIViewController, CAAnimationDelegate {
     
 
@@ -41,41 +39,6 @@ class ClockViewController: UIViewController, CAAnimationDelegate {
 // END: Retrieve time
 
 // MARK: Calculate coordinates of time
-func  timeCoords(x:CGFloat,y:CGFloat,time:(h:Int,m:Int,s:Int),radius:CGFloat,adjustment:CGFloat=90)->(h:CGPoint, m:CGPoint,s:CGPoint) {
-    let cx = x // x origin
-    let cy = y // y origin
-    var r  = radius // radius of circle
-    var points = [CGPoint]()
-    var angle = degree2radian(6)
-    func newPoint (t:Int) {
-        let xpo = cx - r * cos(angle * CGFloat(t)+degree2radian(adjustment))
-        let ypo = cy - r * sin(angle * CGFloat(t)+degree2radian(adjustment))
-        points.append(CGPoint(x: xpo, y: ypo))
-    }
-    // work out hours first
-    var hours = time.h
-    if hours > 12 {
-        hours = hours-12
-    }
-    let hoursInSeconds = time.h*3600 + time.m*60 + time.s
-    newPoint(t: hoursInSeconds*5/3600)
-    
-    // work out minutes second
-    r = radius * 1.25
-    let minutesInSeconds = time.m*60 + time.s
-    newPoint(t: minutesInSeconds/60)
-    // work out seconds last
-    r = radius * 1.5
-    newPoint(t: time.s)
-    
-    return (h:points[0],m:points[1],s:points[2])
-}
-// END: Calculate coordinates of hour
-
-func degree2radian(_ a:CGFloat)->CGFloat {
-    let b = CGFloat(Double.pi) * a/180
-    return b
-}
 
 
 
--- Ready, Set, Draw/TicTacToeView.swift
+++ Ready, Set, Draw/TicTacToeView.swift
@@ -13,39 +13,125 @@ class TicTacToeView: UIView {
 
     override func draw(_ rect: CGRect) {
         
-        //// Bezier Drawing
-        let bezierPath = UIBezierPath()
-        UIColor.black.setStroke()
-        bezierPath.lineWidth = 1
-        bezierPath.fill()
+
         
+       
+        drawBoard()
+        //TOP LEFT
+        drawX(point: CGPoint(x:50,y:60))
+        drawO(point: CGPoint(x:70,y:80))
         
-        //// Rectangle Drawing
-        let rectanglePath = UIBezierPath(rect: CGRect(x: frame.minX + floor(frame.width * 0.32778) + 0.5, y: frame.minY + floor(frame.height * 0.06000) + 0.5, width: floor(frame.width * 0.35000) - floor(frame.width * 0.32778), height: floor(frame.height * 0.92667) - floor(frame.height * 0.06000)))
-        UIColor.black.setStroke()
-        rectanglePath.lineWidth = 1
-        rectanglePath.fill()
+        //TOP CENTER
+        drawX(point: CGPoint(x:165,y:60))
+        drawO(point: CGPoint(x: 185,y: 80))
+
         
+        //TOP RIGHT
+        drawX(point: CGPoint(x:290,y:60))
+        drawO(point: CGPoint(x: 310,y: 80))
+
+        //CENTER LEFT
+        drawX(point: CGPoint(x:50,y:195))
+        drawO(point: CGPoint(x: 70,y: 215))
+
         
-        //// Rectangle 3 Drawing
-        let rectangle3Path = UIBezierPath(rect: CGRect(x: frame.minX + floor(frame.width * 0.68333) + 0.5, y: frame.minY + floor(frame.height * 0.06000) + 0.5, width: floor(frame.width * 0.70556) - floor(frame.width * 0.68333), height: floor(frame.height * 0.92667) - floor(frame.height * 0.06000)))
-        UIColor.black.setStroke()
-        rectangle3Path.lineWidth = 1
-        rectangle3Path.fill()
+        // CENTER CENTER
+        drawX(point: CGPoint(x:165,y:195))
+        drawO(point: CGPoint(x: 185,y: 215))
+
+ 
+        //CENTER RIGHT
+        drawX(point: CGPoint(x:290,y:195))
+        drawO(point: CGPoint(x: 310,y: 215))
+        //BOTTOM LEFT
+        drawX(point: CGPoint(x:50,y:330))
+        drawO(point: CGPoint(x: 70,y: 350))
+       
         
+        // BOTTOM CENTER
+        drawX(point: CGPoint(x:165,y:330))
+        drawO(point: CGPoint(x: 185,y: 350))
+        //        let clock = UIBezierPath()
+        //                clock.addArc(withCenter: CGPoint(x:185, y: 215), radius: 20, startAngle: 0.0, endAngle: 2 * .pi, clockwise: true)
+        //                clock.stroke()
         
-        //// Rectangle 5 Drawing
-        let rectangle5Path = UIBezierPath(rect: CGRect(x: frame.minX + floor(frame.width * 0.07222) + 0.5, y: frame.minY + floor(frame.height * 0.63333) + 0.5, width: floor(frame.width * 0.92778) - floor(frame.width * 0.07222), height: floor(frame.height * 0.66000) - floor(frame.height * 0.63333)))
-        UIColor.black.setStroke()
-        rectangle5Path.lineWidth = 1
-        rectangle5Path.fill()
+        //BOTTOM RIGHT
+        drawX(point: CGPoint(x:290,y:330))
+        drawO(point: CGPoint(x: 310,y: 350))
+
         
-        
-        //// Rectangle 6 Drawing
-        let rectangle6Path = UIBezierPath(rect: CGRect(x: frame.minX + floor(frame.width * 0.07222) + 0.5, y: frame.minY + floor(frame.height * 0.31333) + 0.5, width: floor(frame.width * 0.92778) - floor(frame.width * 0.07222), height: floor(frame.height * 0.34000) - floor(frame.height * 0.31333)))
-        UIColor.black.setStroke()
-        rectangle6Path.lineWidth = 1
-        rectangle6Path.fill()
+
     }
 
+}
+    func drawX(point:CGPoint) -> Void {
+        let x = UIBezierPath()
+        x.move(to: CGPoint(x: point.x, y: point.y))
+        x.addLine(to:CGPoint(x:point.x + 30 ,y: point.y + 30))
+        x.stroke()
+        x.move(to: CGPoint(x: point.x, y:point.y + 30))
+        x.addLine(to: CGPoint(x: point.x + 30,y:point.y))
+        x.stroke()
+    
+}
+func drawO(point:CGPoint) -> Void {
+            let o = UIBezierPath()
+            o.addArc(withCenter: CGPoint(x:point.x, y: point.y), radius: 20, startAngle: 0.0, endAngle: 2 * .pi, clockwise: true)
+            o.stroke()
+    
+}
+//375*434
+func drawBoard() -> Void {
+    //// Bezier Drawing
+    let bezierPath = UIBezierPath()
+    UIColor.black.setStroke()
+    bezierPath.lineWidth = 1
+    bezierPath.fill()
+    //// Rectangle Drawing
+//    let rectanglePath = UIBezierPath(rect: CGRect(x: frame.minX + floor(frame.width * 0.32778) + 0.5, y: frame.minY + floor(frame.height * 0.06000) + 0.5, width: floor(frame.width * 0.35000) - floor(frame.width * 0.32778), height: floor(frame.height * 0.92667) - floor(frame.height * 0.06000)))
+//    UIColor.black.setStroke()
+//    rectanglePath.lineWidth = 1
+//    rectanglePath.fill()
+    
+    bezierPath.move(to: CGPoint(x:125, y:0))
+    bezierPath.addLine(to: CGPoint(x:125, y:430))
+    bezierPath.stroke()
+    
+    bezierPath.move(to: CGPoint(x:250, y:0))
+    bezierPath.addLine(to: CGPoint(x:250, y:430))
+    bezierPath.stroke()
+    
+    bezierPath.move(to: CGPoint(x: 0,y: 145))
+    bezierPath.addLine(to: CGPoint(x:370,y: 145))
+    bezierPath.stroke()
+    
+    bezierPath.move(to: CGPoint(x: 0,y: 290))
+    bezierPath.addLine(to: CGPoint(x:370,y: 290))
+    bezierPath.stroke()
+    
+    
+    
+    
+    
+    
+    
+//    //// Rectangle 3 Drawing
+//    let rectangle3Path = UIBezierPath(rect: CGRect(x: frame.minX + floor(frame.width * 0.68333) + 0.5, y: frame.minY + floor(frame.height * 0.06000) + 0.5, width: floor(frame.width * 0.70556) - floor(frame.width * 0.68333), height: floor(frame.height * 0.92667) - floor(frame.height * 0.06000)))
+//    UIColor.black.setStroke()
+//    rectangle3Path.lineWidth = 1
+//    rectangle3Path.fill()
+//
+//
+//    //// Rectangle 5 Drawing
+//    let rectangle5Path = UIBezierPath(rect: CGRect(x: frame.minX + floor(frame.width * 0.07222) + 0.5, y: frame.minY + floor(frame.height * 0.63333) + 0.5, width: floor(frame.width * 0.92778) - floor(frame.width * 0.07222), height: floor(frame.height * 0.66000) - floor(frame.height * 0.63333)))
+//    UIColor.black.setStroke()
+//    rectangle5Path.lineWidth = 1
+//    rectangle5Path.fill()
+//
+//
+//    //// Rectangle 6 Drawing
+//    let rectangle6Path = UIBezierPath(rect: CGRect(x: frame.minX + floor(frame.width * 0.07222) + 0.5, y: frame.minY + floor(frame.height * 0.31333) + 0.5, width: floor(frame.width * 0.92778) - floor(frame.width * 0.07222), height: floor(frame.height * 0.34000) - floor(frame.height * 0.31333)))
+//    UIColor.black.setStroke()
+//    rectangle6Path.lineWidth = 1
+//    rectangle6Path.fill()
 }
